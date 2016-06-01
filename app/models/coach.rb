e92ff36cfb0e64df019b168a7c5e35621ebd103f
Joseph DePompeis <josephdepompeis@gmail.com>
2016-06-01 17:18:15 -0400

    my teams and conversations updated to reflect multiple coaches on coaches end

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -6,4 +6,26 @@ class Coach < ActiveRecord::Base
 
   has_secure_password
 
+
+
+  def find_user_1
+    User.where(coach_1: self.id)
+  end
+
+  def find_user_2
+    User.where(coach_2: self.id)
+  end
+
+  def find_user_3
+    User.where(coach_3: self.id)
+  end
+
+  def find_user_4
+    User.where(coach_4: self.id)
+  end
+
+  def find_user_5
+    User.where(coach_5: self.id)
+  end
+
 end
