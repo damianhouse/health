014954ac9f0cdf36402c84915848a048d00edf76
Joseph DePompeis <josephdepompeis@gmail.com>
2016-06-10 10:53:57 -0400

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILGUN_SMTP_SERVER'],
     port:                 ENV['MAILGUN_SMTP_PORT'],
-    domain:               ENV["MAILGUN_SANDBOX_DOMAIN"],
+    domain:               'healthstyle.herokuapp.com',
     user_name:            ENV['MAILGUN_SMTP_LOGIN'],
     password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
