c72eb190f14196c37d8badb84a1d7c9cd0a726c2
Joseph DePompeis <josephdepompeis@gmail.com>
2016-06-10 14:45:30 -0400

    added body for text msg

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,7 +7,12 @@ class NotificationsController < ApplicationController
     unless user.phone == ""
       begin
         client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-        message = client.messages.create from: '8284820730', to: user.phone, body: 'You are receiving this message from Healthstyle!'
+        message = client.messages.create from: '8284820730', to: user.phone,
+
+        body: 'We’ve got wonderful news! Your three HealthStyle Coaches are ready for you.
+                Just go to MyHealthStyleapp.com and log in to meet your new HealthStyle Coaches and get started on
+                your way to reaching your personal goals.
+                And thank you for joining My HealthStyle, Inc.'
         redirect_to abouts_signupconfirmation_path
       rescue
         redirect_to :back
@@ -17,4 +22,11 @@ class NotificationsController < ApplicationController
       redirect_to abouts_signupconfirmation_path
     end
   end
+
+
+
+
+
+
+
 end
