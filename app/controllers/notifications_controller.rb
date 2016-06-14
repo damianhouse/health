4d6b568916b89c06e2d162fcb76e1d4fd9f69794
Joseph DePompeis <josephdepompeis@gmail.com>
2016-06-14 11:15:27 -0400

    text msg copy swap

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,10 +9,7 @@ class NotificationsController < ApplicationController
         client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
         message = client.messages.create from: '8284820730', to: user.phone,
 
-        body: 'We’ve got wonderful news! Your three HealthStyle Coaches are ready for you.
-                Just go to MyHealthStyleapp.com and log in to meet your new HealthStyle Coaches and get started on
-                your way to reaching your personal goals.
-                And thank you for joining My HealthStyle, Inc.'
+        body: 'Welcome to My HealthStyle! We are so excited and will contact you as soon as your coaching team is assigned to you!'
         redirect_to abouts_signupconfirmation_path
       rescue
         redirect_to :back
@@ -27,7 +24,11 @@ class NotificationsController < ApplicationController
     @user = User.find_by(email: params[:email])
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
     message = client.messages.create from: '8284820730', to: @user.phone,
-    body: 'Hooray! We have your team! Log back in to MyHealthStyleApp.com and begin a conversation, or if you\'re shy they\'ll be reaching out to you very soon! '
+
+    body: 'We’ve got wonderful news! Your three HealthStyle Coaches are ready for you.
+            Just go to MyHealthStyleapp.com and log in to meet your new HealthStyle Coaches and get started on
+            your way to reaching your personal goals.
+            And thank you for joining My HealthStyle, Inc.'
   end
 
 end
