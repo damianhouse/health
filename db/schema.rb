7718f29030b7f770697e50fe041dbabfbe7d8c23
mkg1 <gehring.mg@gmail.com>
2016-06-18 12:37:43 -0400

    Add last name column to user db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608152702) do
+ActiveRecord::Schema.define(version: 20160618162704) do
 
   create_table "coaches", force: :cascade do |t|
     t.string   "name"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160608152702) do
     t.string   "avatar_url"
     t.string   "phone"
     t.string   "zip"
+    t.string   "last_name"
   end
 
 end
