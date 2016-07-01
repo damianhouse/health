3d947d85a9e6971223a411ff29814fe39618d67f
mkg1 <gehring.mg@gmail.com>
2016-07-01 16:19:02 -0400

    Add email validation for coaches

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,4 +1,5 @@
 class Coach < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   has_many :users
   has_many :messages
   has_many :notes
