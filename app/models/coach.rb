7a149ec6ccca295e58b441e197aa18471f753327
mkg1 <gehring.mg@gmail.com>
2016-07-05 10:39:10 -0400

    Add email validation for coach

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,5 +1,4 @@
 class Coach < ActiveRecord::Base
-  before_validation :downcase_email
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   has_many :users
   has_many :messages
