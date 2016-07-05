f5f900a74b8c2edbfa5e001684507de72b4b32d8
mkg1 <gehring.mg@gmail.com>
2016-07-05 15:11:34 -0400

    Log coaches in after creating account

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -30,6 +30,7 @@ class CoachesController < ApplicationController
 
     respond_to do |format|
       if @coach.save
+        session[:coach_id] = @coach.id
         format.html { redirect_to @coach, notice: 'Coach was successfully created.' }
         format.json { render :show, status: :created, location: @coach }
       else
