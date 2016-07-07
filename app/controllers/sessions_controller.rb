bb56a1b8910d954f16fc300552690417d2cc7be4
mkg1 <gehring.mg@gmail.com>
2016-07-07 14:57:16 -0400

    Redirect users to team page after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,19 +8,19 @@ class SessionsController < ApplicationController
 
         if coach && coach.authenticate(params[:password])
           session[:coach_id] = coach.id
-          redirect_to coaches_path, notice: "Login successful."
+          redirect_to teams_index_path, notice: "Login successful."
 
         elsif user && user.authenticate(params[:password])
           session[:user_id] = user.id
-          redirect_to users_path, notice: "Login successful."
+          redirect_to teams_index_path, notice: "Login successful."
 
         else
           flash.now[:notice] = "Either your password or email are invalid."
         end
       elsif session[:user_id] != nil
-        redirect_to users_path
+        redirect_to teams_index_path
       elsif session[:coach_id] != nil
-        redirect_to coaches_path
+        redirect_to teams_index_path
       end
 
 
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
     def logout
       session[:user_id] = nil
       session[:coach_id] = nil
-      
+
       redirect_to sessions_login_path, alert: "You've beens successfully logged out. Check back soon!"
     end
 
