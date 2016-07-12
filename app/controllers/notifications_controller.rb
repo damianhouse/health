59e58fed655bc8f42f784927cfa0604f06ad79c5
mkg1 <gehring.mg@gmail.com>
2016-07-12 00:01:57 -0400

    Change MHS to all one word

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,7 +9,7 @@ class NotificationsController < ApplicationController
         client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
         message = client.messages.create from: '8284820730', to: user.phone,
 
-        body: 'Welcome to My HealthStyle! We are so excited and will contact you as soon as your coaching team is assigned to you!'
+        body: 'Welcome to MyHealthStyle! We are so excited and will contact you as soon as your coaching team is assigned to you!'
         redirect_to abouts_signupconfirmation_path
       rescue
         redirect_to :back
@@ -28,7 +28,7 @@ class NotificationsController < ApplicationController
     body: 'We’ve got wonderful news! Your three HealthStyle Coaches are ready for you.
             Just go to MyHealthStyleapp.com and log in to meet your new HealthStyle Coaches and get started on
             your way to reaching your personal goals.
-            And thank you for joining My HealthStyle, Inc.'
+            And thank you for joining MyHealthStyle, Inc.'
   end
 
 end
