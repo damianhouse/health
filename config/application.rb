59e4aab83a6b5ae77ec23a28df1f8e17295ff86e
mkg1 <gehring.mg@gmail.com>
2016-07-13 15:29:32 -0400

    Attempt at fixing compiler issues

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Health
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+    Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
+    Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   end
 end
