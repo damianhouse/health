0e2040038af52cb15b3ae9f9959755008ca9ec68
damianhouse <damianhouse@gmail.com>
2016-07-26 11:07:10 -0400

    Messages that have been seen are marked as read.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 class ConversationsController < ApplicationController
   before_action :set_conversation, only: [:show, :edit, :update, :destroy]
   before_action :logged_in?, only: [:show, :index]
+  before_action :verify_user_or_coach_or_admin?, only: [:show]
   # GET /conversations
   # GET /conversations.json
   def index
@@ -13,10 +14,10 @@ class ConversationsController < ApplicationController
     if @current_user && (@current_user.id == @conversation.coach_id || @current_user.id == @conversation.user_id)
       @message = Message.new
 
-      if session[:user_id] != nil
+      if session[:user_id]
         @profile = @conversation.coach
         @perspective = "user"
-      else
+      elsif session[:coach_id]
         @profile = @conversation.user
         @perspective = "coach"
       end
@@ -25,15 +26,11 @@ class ConversationsController < ApplicationController
 
     #mark conversations read after user/coach views
       @conversation.messages.each do |msg|
-        if session[:coach_id] != nil
-            if msg.user_id != nil
-              msg.read = true
-              msg.save!
-            end
-        elsif session[:user_id] != nil
-          if msg.coach_id != nil
-            msg.read = true
-            msg.save!
+        if msg.read.nil?
+          if session[:coach_id]
+            (msg[:read] = true) && msg.save! unless msg.user != nil
+          elsif session[:user_id]
+            (msg[:read] = true) && msg.save! unless msg.coach != nil
           end
         end
       end
@@ -96,6 +93,18 @@ class ConversationsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_conversation
       @conversation = Conversation.find(params[:id])
+    end
+
+    def verify_user_or_coach_or_admin?
+      if session[:admin]
+        conversation_path
+      elsif session[:coach_id]
+        conversation_path if (@conversation.coach == Coach.find(session[:coach_id]))
+      elsif session[:user_id]
+        conversation_path if (@conversation.user == User.find(session[:user_id]))
+      else
+        render json: "You do not have permission to access this page."
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
