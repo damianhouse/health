2d60cd8f8a6e3f1901898e2a196fba4269ab2dac
damianhouse <damianhouse@gmail.com>
2016-08-21 14:15:01 -0400

    Add a link to the text message sent to the users to go to their teams. Add pinterest meta tag for analytics.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -41,7 +41,7 @@ class NotificationsController < ApplicationController
     message = client.messages.create from: '8284820730', to: @user.phone,
 
     body: 'We’ve got wonderful news! Your three HealthStyle Coaches are ready for you.
-            Just go to MyHealthStyleapp.com and log in to meet your new HealthStyle Coaches and get started on
+            Just go to http://www.myhealthstyleapp.com/teams/index and log in to meet your new HealthStyle Coaches and get started on
             your way to reaching your personal goals.
             And thank you for joining MyHealthStyle, Inc.'
   end
