1910cf51a571c2fc6578b30d75b22a727e9489d6
damianhouse <damianhouse@gmail.com>
2016-09-10 14:05:50 -0400

    Refactor coach's team page for less code and made both users and coach's Messages text clickable.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,6 +8,9 @@ before_action :logged_in?
          @secondary_coach = User.all.where("coach_1 = ? OR coach_2 = ? OR coach_3 = ? OR coach_4 = ?", @coach.id, @coach.id, @coach.id, @coach.id)
        elsif session[:user_id] != nil
         @user = User.find(session[:user_id])
+        @secondary_coaches = []
+        @secondary_coaches << Coach.find(@user.coach_1)
+        @secondary_coaches << Coach.find(@user.coach_2)
        end
       @conversation = Conversation.new
       @note = Note.new
